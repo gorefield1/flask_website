fc8d849b32f33dcb2eb64d1df895651e6d51ab71
gorefield1 <efremenkovsergej992@gmail.com>
2025-10-17 14:48:04 +0200

    add file

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return "index"
+
+@app.route('/about')
+def about():
+    return render_template("shablon.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
