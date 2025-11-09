82cf6dee4911dd7ff05e797d0e4429a65bb4a20b
gorefield1 <efremenkovsergej992@gmail.com>
2025-11-09 10:02:02 +0100

    add file

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+dzindza_menu = ['download', 'feedback', 'my websites']
+
 @app.route('/')
 def index():
     return "index"
 
 @app.route('/about')
 def about():
-    return render_template("shablon.html")
+    return render_template("shablon.html", title='dzindza module', menu=dzindza_menu)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
